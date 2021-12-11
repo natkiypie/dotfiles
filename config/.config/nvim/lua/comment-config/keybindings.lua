95b75c20cf63b592805ea1de83e6b442a9888e2b
natkiypie <natkiypie@protonmail.com>
2021-12-10 20:11:27 -0800

    Update depreciated lsp diagnostic methods.

--- config/.config/nvim/lua/comment-config/keybindings.lua
+++ config/.config/nvim/lua/comment-config/keybindings.lua
@@ -3,10 +3,11 @@ local keys = require'utils.keybindings'
 keys.bind_normal_mode {
   {'<C-_>', '<CMD>CommentToggle<CR>'},
   {'<C-_><C-_>', 'vip:CommentToggle<CR>'},
-  {'cp', 'yy:CommentToggle<CR>p'},
+  {'<C-c><C-l>', 'yy:CommentToggle<CR>p'},
   {'<C-c><C-p>', '}o<ESC>2kvipyvip:CommentToggle<CR>}p'},
 }
 
 keys.bind_visual_mode {
   {'<C-_>', ':CommentToggle<CR>'},
+  {'<C-c><C-p>', 'yv`]:CommentToggle<CR>`]p'},
 }
--- config/.config/nvim/lua/lsp-config/keybindings.lua
+++ config/.config/nvim/lua/lsp-config/keybindings.lua
@@ -7,31 +7,33 @@ B.buf_map = function(bufnr, mode, lhs, rhs, opts)
 end
 
 B.on_attach = function(client, bufnr)
-    vim.cmd('command! LspDef lua vim.lsp.buf.definition()')
-    vim.cmd('command! LspFormatting lua vim.lsp.buf.formatting()')
-    vim.cmd('command! LspCodeAction lua vim.lsp.buf.code_action()')
-    vim.cmd('command! LspHover lua vim.lsp.buf.hover()')
-    vim.cmd('command! LspRename lua vim.lsp.buf.rename()')
-    vim.cmd('command! LspRefs lua vim.lsp.buf.references()')
-    vim.cmd('command! LspTypeDef lua vim.lsp.buf.type_definition()')
-    vim.cmd('command! LspImplementation lua vim.lsp.buf.implementation()')
-    vim.cmd('command! LspDiag lua vim.diagnostic.show()')
-    vim.cmd('command! LspDiagLine lua vim.diagnostic.open_float()')
-    vim.cmd('command! LspSignatureHelp lua vim.lsp.buf.signature_help()')
-    vim.cmd('command! LspSignatureHelp lua vim.lsp.buf.signature_help()')
-    B.buf_map(bufnr, 'n', 'gd', ':LspDef<CR>')
-    B.buf_map(bufnr, 'n', 'gr', ':LspRename<CR>')
-    B.buf_map(bufnr, 'n', 'gy', ':LspTypeDef<CR>')
-    B.buf_map(bufnr, 'n', '<Leader>k', ':LspHover<CR>')
-    B.buf_map(bufnr, 'n', '[a', ':LspDiag<CR>')
-    B.buf_map(bufnr, 'n', ']a', ':LspDiag<CR>')
-    B.buf_map(bufnr, 'n', 'ga', ':LspCodeAction<CR>')
-    B.buf_map(bufnr, 'n', '<Leader>d', ':LspDiagLine<CR>')
-    B.buf_map(bufnr, 'n', '<Leader>r', '<CMD>LspRestart<CR>')
-    B.buf_map(bufnr, 'i', '<C-z>', '<CMD>LspSignatureHelp<CR>')
-    if client.resolved_capabilities.document_formatting then
-        vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()')
-    end
+  vim.cmd[[
+    command! LspDef lua vim.lsp.buf.definition()
+    command! LspFormatting lua vim.lsp.buf.formatting()
+    command! LspCodeAction lua vim.lsp.buf.code_action()
+    command! LspHover lua vim.lsp.buf.hover()
+    command! LspRename lua vim.lsp.buf.rename()
+    command! LspRefs lua vim.lsp.buf.references()
+    command! LspTypeDef lua vim.lsp.buf.type_definition()
+    command! LspImplementation lua vim.lsp.buf.implementation()
+    command! LspDiag lua vim.diagnostic.show()
+    command! LspDiagLine lua vim.diagnostic.open_float()
+    command! LspSignatureHelp lua vim.lsp.buf.signature_help()
+    command! LspSignatureHelp lua vim.lsp.buf.signature_help()
+  ]]
+  B.buf_map(bufnr, 'n', 'gd', ':LspDef<CR>')
+  B.buf_map(bufnr, 'n', 'gr', ':LspRename<CR>')
+  B.buf_map(bufnr, 'n', 'gy', ':LspTypeDef<CR>')
+  B.buf_map(bufnr, 'n', '<Leader>k', ':LspHover<CR>')
+  B.buf_map(bufnr, 'n', '[a', ':LspDiag<CR>')
+  B.buf_map(bufnr, 'n', ']a', ':LspDiag<CR>')
+  B.buf_map(bufnr, 'n', 'ga', ':LspCodeAction<CR>')
+  B.buf_map(bufnr, 'n', '<Leader>d', ':LspDiagLine<CR>')
+  B.buf_map(bufnr, 'n', '<Leader>r', '<CMD>LspRestart<CR>')
+  B.buf_map(bufnr, 'i', '<C-z>', '<CMD>LspSignatureHelp<CR>')
+  if client.resolved_capabilities.document_formatting then
+      vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()')
+  end
 end
 
 return B
