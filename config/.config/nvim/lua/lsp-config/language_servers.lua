99008a2f5c719cc7a002229a970c6e3937043674
natkiypie <natkiypie@protonmail.com>
2021-12-10 17:08:02 -0800

    Clean up lsp language-servers file - Progress.

--- config/.config/nvim/lua/lsp-config/language_servers.lua
+++ config/.config/nvim/lua/lsp-config/language_servers.lua
@@ -28,36 +28,34 @@ local langservers = {
   'cssls',
   'sumneko_lua'
 }
-local sumneko_cmd = {sumneko_binary, '-E', sumneko_root_path .. '/main.lua'}
-local sumneko_settings = {
-  Lua = {
-    runtime = {
-      -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-      version = 'LuaJIT',
-      -- Setup your lua path
-      path = runtime_path,
+
+local sumneko_capabilities = {
+  cmd = {sumneko_binary, '-E', sumneko_root_path .. '/main.lua'},
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+        path = runtime_path,
+      },
+      diagnostics = {
+        globals = {'vim'},
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file('', true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
     },
-    diagnostics = {
-      -- Get the language server to recognize the `vim` global
-      globals = {'vim'},
-    },
-    workspace = {
-      -- Make the server aware of Neovim runtime files
-      library = vim.api.nvim_get_runtime_file('', true),
-      checkThirdParty = false,
-    },
-    -- Do not send telemetry data containing a randomized but unique identifier
-    telemetry = {
-      enable = false,
-    },
-  },
+  }
 }
 
 for _, server in ipairs(langservers) do
   if server == 'sumneko_lua' then
     require'lspconfig'[server].setup {
-      cmd = sumneko_cmd;
-      settings = sumneko_settings,
+      cmd = sumneko_capabilities.cmd;
+      settings = sumneko_capabilities.settings
     }
   else
     require'lspconfig'[server].setup {
