594be3c5d0d90f43968493529be71bbb23bee5f0
natkiypie <natkiypie@protonmail.com>
2021-12-15 03:50:57 -0800

    Return glow script to og state.

--- config/.config/scripts/glow_script.sh
+++ config/.config/scripts/glow_script.sh
@@ -2,8 +2,7 @@
 
 [ -d "/tmp/glow" ] && rm /tmp/glow/* || mkdir /tmp/glow
 
-# find ../../ -path *node_modules* -prune -o -iname *.md -exec cp -ar {} /tmp/glow \;
-find ./ -path *node_modules* -prune -o -iname *.md -exec cp -ar {} /tmp/glow \;
+find ../../ -path *node_modules* -prune -o -iname *.md -exec cp -ar {} /tmp/glow \;
 
 glow /tmp/glow
 
