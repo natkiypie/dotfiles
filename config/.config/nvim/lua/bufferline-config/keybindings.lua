cd4602270215c133c07a20b6eca414413f583add
natkiypie <natkiypie@protonmail.com>
2021-12-17 23:10:10 -0800

    Make bufferlinepick function that dims highlight of selected buffer ﬘

--- config/.config/nvim/lua/bufferline-config/keybindings.lua
+++ config/.config/nvim/lua/bufferline-config/keybindings.lua
@@ -1,5 +1,3 @@
-local bind_normal_mode = require('utils.keybindings').bind_normal_mode
-
-bind_normal_mode {
-  { 'm', '<CMD>BufferLinePick<CR>' },
+require('utils.keybindings').bind_normal_mode {
+  { 'm', '<CMD>lua require"bufferline-config.utils".bufferline_pick()<CR>' },
 }
---
+++ config/.config/nvim/lua/bufferline-config/utils.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+M.bufferline_pick = function()
+  vim.cmd [[
+    hi BufferLineBufferSelected gui=none guifg=#515a82
+    hi BufferLineBackground guifg=#a9b1d6
+  ]]
+  vim.cmd [[
+    BufferLinePick
+    hi BufferLineBufferSelected gui=none guifg=#a9b1d6
+    hi BufferLineBackground guifg=#515a82
+  ]]
+end
+
+return M
