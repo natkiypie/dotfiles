42eae881659c99ac9640abbc1120912e9fca0827
natkiypie <natkiypie@protonmail.com>
2021-12-31 02:18:53 -0800

    style(ftplugin): convert ftplugin files from vimscript to lua

--- config/.config/nvim/after/ftplugin/json.vim
+++
@@ -1,4 +0,0 @@
-setlocal formatoptions-=cro
-set softtabstop=2
-set shiftwidth=2
-set expandtab
---
+++ config/.config/nvim/after/ftplugin/toml.lua
@@ -0,0 +1,4 @@
+vim.opt_local.formatoptions = vim.opt_local.formatoptions - { 'c', 'r', 'o' }
+vim.bo.softtabstop = 2
+vim.bo.shiftwidth = 2
+vim.bo.expandtab = true
