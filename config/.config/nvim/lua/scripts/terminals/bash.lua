f5fb5db8a67982c5647723b77c11cf8e29090691
natkiypie <natkiypie@protonmail.com>
2022-01-01 13:10:12 -0800

    refactor(bash terminal): break up toggle function

--- config/.config/nvim/lua/scripts/terminals/bash.lua
+++ config/.config/nvim/lua/scripts/terminals/bash.lua
@@ -1,51 +1,50 @@
 local M = {}
 
-function M.toggle()
+local function initialize_terminal()
+  M.terminal = {
+    originbufferid = nil,
+    termbufferid = nil,
+  }
+end
+
+local function open()
   if vim.fn.has 'nvim' == 0 then
     return false
   end
 
-  local function initialize_terminal()
-    M.terminal = {
-      loaded = false,
-      originbufferid = nil,
-      termbufferid = nil,
-    }
-  end
-
-  if not M.terminal then
-    initialize_terminal()
-  end
+  initialize_terminal()
 
   M.terminal.on_exit = function(_, code)
     if code == 0 or code == 130 then
       buffer(M.terminal.originbufferid)
       exec_arg('Bclose!', M.terminal.termbufferid)
-      initialize_terminal()
+      M.terminal = nil
       require('utils.general').close_tab()
     end
   end
 
-  if not M.terminal.loaded then
-    require('utils.general').split_tab()
-    M.terminal.originbufferid = vim.fn.bufnr ''
-    vim.api.nvim_command 'enew'
-    vim.fn.termopen('/bin/bash', M.terminal)
-    vim.bo.ft = 'terminal'
-    vim.cmd 'startinsert'
-    M.terminal['loaded'] = true
-    M.terminal['termbufferid'] = vim.fn.bufnr ''
-    return true
-  end
+  require('utils.general').split_tab()
+  M.terminal.originbufferid = vim.fn.bufnr ''
+  vim.api.nvim_command 'enew'
+  vim.fn.termopen('/bin/bash', M.terminal)
+  vim.bo.ft = 'terminal'
+  vim.cmd 'startinsert'
+  M.terminal['termbufferid'] = vim.fn.bufnr ''
+end
 
-  if M.terminal.termbufferid == vim.fn.bufnr '' then
-    require('utils.general').close_tab()
-    buffer(M.terminal.originbufferid)
+function M.toggle()
+  if not M.terminal then
+    open()
   else
-    require('utils.general').split_tab()
-    M.terminal.originbufferid = vim.fn.bufnr ''
-    buffer(M.terminal.termbufferid)
-    vim.cmd 'startinsert'
+    if M.terminal.termbufferid == vim.fn.bufnr '' then
+      require('utils.general').close_tab()
+      buffer(M.terminal.originbufferid)
+    else
+      require('utils.general').split_tab()
+      M.terminal.originbufferid = vim.fn.bufnr ''
+      buffer(M.terminal.termbufferid)
+      vim.cmd 'startinsert'
+    end
   end
 end
 
