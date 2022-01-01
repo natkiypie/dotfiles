743e30377274c6b146f7a0abfc35dc3875a35748
natkiypie <natkiypie@protonmail.com>
2021-12-31 17:20:03 -0800

    style(broot): change selection line and help menu skin colors

--- config/.config/nvim/lua/telescope-config/highlights.lua
+++ config/.config/nvim/lua/telescope-config/highlights.lua
@@ -3,6 +3,7 @@ vim.cmd [[
   hi TelescopeResultsBorder guifg=#a9b1d6
   hi TelescopePreviewBorder guifg=#a9b1d6
   hi TelescopePromptTitle guifg=#a9b1d6
+  hi TelescopePromptNormal guibg=none
   hi TelescopeResultsTitle guifg=#a9b1d6
   hi TelescopePreviewTitle guifg=#a9b1d6
   hi TelescopeResultsNormal guifg=#a9b1d6
