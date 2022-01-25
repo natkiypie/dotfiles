78aef407a6e6903b7c926dca36710e10cd5e21d2
natkiypie <natkiypie@protonmail.com>
2022-01-24 16:40:09 -0800

    feat(scripts): reset background image after configuring external monitor

--- .scripts/ex_mon_on.sh
+++ .scripts/ex_mon_on.sh
@@ -3,5 +3,4 @@
 # setup any external monitors
 hdmi_active=$(xrandr |grep 'DP-3' | grep -w 'connected' | awk '{print $1}')
 
-#[[ ! -z "$hdmi_active" ]] && xrandr --output DP-3 --auto --pos 1920x0 --output eDP-1 --auto --pos 0x433
-[[ ! -z "$hdmi_active" ]] && xrandr --output DP-3 --auto --pos 163x0 --output eDP-1 --auto --pos 0x900
+[[ ! -z "$hdmi_active" ]] && xrandr --output DP-3 --auto --pos 163x0 --output eDP-1 --auto --pos 0x900 && set_background_img.sh
