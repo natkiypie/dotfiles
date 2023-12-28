d76dc43b26c8607b2143445e16ac9175b29dc4e6
natkiypie <natkiypie@protonmail.com>
2023-12-28 00:28:24 -0700

    Alphabetize autocommands.

--- .config/nvim/lua/custom/autocmd.lua
+++ .config/nvim/lua/custom/autocmd.lua
@@ -1,22 +1,5 @@
 local autocmd = vim.api.nvim_create_autocmd
 
---Nvim-Comment
-autocmd('BufEnter', {
-  desc = 'Prevent auto comment new line',
-  callback = function()
-    vim.opt_local.formatoptions = vim.opt_local.formatoptions - { 'c', 'r', 'o' }
-  end,
-})
-
---LSP
-autocmd('LspAttach', {
-  desc = 'Fix semantic tokens for lsp',
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    client.server_capabilities.semanticTokensProvider = nil
-  end,
-})
-
 --Leap
 autocmd('User', {
   pattern = 'LeapEnter',
@@ -26,10 +9,27 @@ autocmd('User', {
   end,
 })
 
+--LSP
+autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+  desc = 'Fix semantic tokens for lsp',
+})
+
 autocmd('User', {
   pattern = 'LeapLeave',
   callback = function()
     vim.cmd.hi('Cursor', 'blend=0')
     vim.opt.guicursor:remove { 'a:Cursor/lCursor' }
   end,
+})
+
+--Nvim-Comment
+autocmd('BufEnter', {
+  callback = function()
+    vim.opt_local.formatoptions = vim.opt_local.formatoptions - { 'c', 'r', 'o' }
+  end,
+  desc = 'Prevent auto comment new line',
 })
