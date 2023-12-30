ce0f5f598972515f397696f9c26d649704ca6dc6
natkiypie <natkiypie@protonmail.com>
2023-12-29 19:48:35 -0700

    Set option to prevent folds.

--- .config/nvim/lua/custom/options.lua
+++ .config/nvim/lua/custom/options.lua
@@ -4,6 +4,7 @@ vim.go.timeoutlen = 150
 vim.go.laststatus = 0
 vim.go.mouse = ''
 vim.go.guicursor = [[i:block,v:block-vCursor]]
+vim.wo.foldenable = false
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.bo.softtabstop = 2
