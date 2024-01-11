3d082cebf5c49f568dab243eac845f250cbb7ba3
natkiypie <natkiypie@protonmail.com>
2024-01-11 01:48:31 -0700

    Continue configuring Neovim to program in OCaml.

--- .config/nvim/lua/custom/configs/none-ls.lua
+++ .config/nvim/lua/custom/configs/none-ls.lua
@@ -6,38 +6,30 @@ local null_ls_utils = require 'null-ls.utils'
 
 mason_null_ls.setup {
   ensure_installed = {
-    'prettier', -- prettier formatter
-    'stylua', -- lua formatter
-    'eslint_d', -- js linter
+    'prettier',
+    'stylua',
+    'eslint_d',
+    'ocamlformat',
   },
 }
 
--- for conciseness
-local formatting = null_ls.builtins.formatting -- to setup formatters
-local diagnostics = null_ls.builtins.diagnostics -- to setup linters
-
--- to setup format on save
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
--- configure null_ls
 null_ls.setup {
-  -- add package.json as identifier for root (for typescript monorepos)
   root_dir = null_ls_utils.root_pattern('.null-ls-root', 'Makefile', '.git', 'package.json'),
-  -- setup formatters & linters
   sources = {
-    --  to disable file types use
-    --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-    formatting.prettier.with {
-      extra_filetypes = { 'svelte' },
-    }, -- js/ts formatter
-    formatting.stylua, -- lua formatter
-    diagnostics.eslint_d.with { -- js/ts linter
+    formatting.prettier,
+    formatting.stylua,
+    formatting.ocamlformat,
+    diagnostics.eslint_d.with {
       condition = function(utils)
         return utils.root_has_file { '.eslintrc.js', '.eslintrc.cjs' } -- only enable if root has .eslintrc.js or .eslintrc.cjs
       end,
     },
   },
-  -- configure format on save
+
   on_attach = function(current_client, bufnr)
     if current_client.supports_method 'textDocument/formatting' then
       vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
@@ -47,7 +39,6 @@ null_ls.setup {
         callback = function()
           vim.lsp.buf.format {
             filter = function(client)
-              --  only use null-ls for formatting instead of lsp server
               return client.name == 'null-ls'
             end,
             bufnr = bufnr,
